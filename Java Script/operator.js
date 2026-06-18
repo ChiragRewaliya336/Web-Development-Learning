558965763eb5b5738df21c5a05a62ac7cb6e7896
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-18 17:28:41 +0530

    Day 15 Arithmetric Operator

---
+++ Java Script/operator.js
@@ -0,0 +1,13 @@
+let a =10;
+let b=5;
+// Arithemetic Operator:
+ /* console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**3); */ // Power
+
+// NaN =Not a number 0/0 = Nan;
+/*console.log(typeof NaN);
+console.log( NaN +1); */
