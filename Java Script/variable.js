56a03bbe6e814d4d7ccb8ca578bc9e7e79f26a37
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-18 17:04:40 +0530

    Variables in js

---
+++ Java Script/variable.js
@@ -0,0 +1,29 @@
+const accountID =14553
+let accountEmail = "chirag@gmail.com"
+var accountPass =12345
+accountCity ="Jaipur"
+let accountstate = null
+// accountID =2 
+
+console.log(typeof accountPass);
+
+accountEmail="Ck@gmail.com"
+accountPass ="2121"
+accountCity= "Haryana"
+// console.log(accountID);
+// console.table([accountID,accountEmail,accountCity,accountPass,accountstate])
+// alert(3+3) //we are using node js note browser
+
+let name ="chirag"
+let age =18
+let isLogged =false
+/* Primitive Datatypes
+    number=> 2 to power 53
+    null=>standlone value
+    undefined=>
+    string=>""
+    symbol=> unique
+    bigInt
+*/
+// console.log(typeof "chirag");
+
