b9a85d05534cc9af76222907b5063de2a522c3b3
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-03 16:57:57 +0530

    Complete pending of function

--- Java Script/function.js
+++ Java Script/function.js
@@ -38,4 +38,28 @@ function outerfn(){
  let sum = function(a,b){
     return a+b;
  }
- console.log(sum(1, 2)); 
+//  console.log(sum(1, 2)); 
+
+ function multiplegreet(func, count){
+    for(let i=1; i<=count; i++){
+        func();
+    }
+ }
+ let greet = function(){
+    console.log("hello");
+ }
+//  multiplegreet(greet,10);
+ const calc = {
+    num:55,
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){ //sub(a,b){   }
+        return a-b;
+    },
+    pro: function(a,b){
+        return a*b;
+    }
+ };
+console.log(calc.add(1,2));
+
