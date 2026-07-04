2fa1cc7d6c9c8578aac400f245919e814664cbed
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-04 18:54:14 +0530

    Arrow functions complete

--- Java Script/arrow.js
+++ Java Script/arrow.js
@@ -9,4 +9,84 @@ const student ={
         console.log(avg);
     }
 };
-console.log(student.getavg());
+// console.log(student.getavg());
+
+// Try and Catch system:
+// try{
+//     console.log(a); 
+// } catch(e){
+//     console.log("Variable is not exist");
+//     console.log(e);
+    
+// }
+
+// Arrow Function:
+const sum = (a,b) =>{
+    console.log(a+b);
+}
+const pow = (a,b) =>{
+    console.log(a**b);
+}
+const hello = () =>{
+    console.log("hello");
+}
+
+// sum(8,4);
+// pow(2,4);
+// console.log(sum);
+
+// implicit return in arrow function:
+const mul = (a,b) =>(
+    a*b   //    can run without return statement.
+)
+const prod = (a,b) => a*b   //runn valid
+// console.log(mul(5,4));
+
+// Set Timeout function:
+// console.log("Hi, i am chirag");
+// setTimeout( () =>{
+//     console.log("waiting...");
+    
+// },4000)
+// console.log("Thanks");
+
+
+// Set invertal:
+// let id= setInterval( () =>{
+//     console.log("waiting...");
+// },2000)
+// console.log(id);
+// let id2= setInterval( () =>{
+//     console.log("waiting2...");
+// },2000)
+// console.log(id2);
+// clearInterval(id2)  For stoping.
+
+
+// This with arrow function:
+const Student ={
+    name: "Chirag",
+    marks: 90,
+    prop: this, //Global scope
+    getName: function(){
+        console.log(this); 
+        return this.name;
+    },
+    getMarks: () =>{
+        console.log(this); //Parents's scope -.Window 
+        return this.marks;
+    },
+    getInfo1: function(){
+        setTimeout( () =>{
+            console.log(this); //student
+        },2000)
+    },
+    getInfo2: function(){
+        setTimeout( function() {
+            console.log(this); //window
+        },2000)
+    },
+};
+console.log(Student.getInfo1());
+console.log(Student.getInfo2());
+
