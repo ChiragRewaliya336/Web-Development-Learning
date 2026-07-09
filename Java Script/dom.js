8853b29046149df9cadf25b8318399187a7e3441
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-09 16:42:31 +0530

    :( just st dom

---
+++ Java Script/dom.js
@@ -0,0 +1,14 @@
+// document.all[8].innerHTML="Dog"
+// 'Dog'
+// document.getElementById("mainImg");
+// mainImg.src;
+// console.dir(mainImg);
+// document.getElementsByClassName("box");
+// document.getElementsByClassName("box")[0];
+
+let smallimg = document.getElementsByClassName("oldImg");
+
+for (let i = 0; i < smallimg.length; i++) {
+    smallimg[i].src = "https://picsum.photos/id/237/500/350";
+    console.log(`Value of image no. ${i} is changed.`);
+}
